38ac9f32c10e3576c07f98dda633848c650caf72
Aaron Craig <aaron@temptster.com>
2013-10-29 15:51:24 +0000

    aaron

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,7 @@
 var Wishable = Wishable || {
   initialized:      false,
   cordova_present:  (typeof window.cordova != 'undefined'),
+  page_params:      {},
 
   init:   function init($, $context) {
     $('#create-wish-form').validate({
@@ -8,7 +9,10 @@ var Wishable = Wishable || {
         var params = $(form).formParams();
         Wishable.api.create_wish(params, function(data) {
           if (data) {
-            alert('your wish has been created');
+            Wishable.change_page('#wish', {wish_id: data.id});
+          }
+          else {
+            Wishable.show_last_error();
           }
         });
       }
@@ -21,6 +25,9 @@ var Wishable = Wishable || {
           if (data) {
             alert('your account has been created');
           }
+          else {
+            Wishable.show_last_error();
+          }
         });
       }
     });
@@ -31,6 +38,9 @@ var Wishable = Wishable || {
         Wishable.api.login(params, function(data) {
           if (data) {
             alert('you have logged in');
+          }
+          else {
+            Wishable.show_last_error();
           }
         });
       }
@@ -62,6 +72,44 @@ var Wishable = Wishable || {
     }
   },
 
+  query_value: function query_value(name) {
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+
+    if (!results) {
+      return null;
+    }
+    return results[1] || null;
+  },
+
+  change_page: function change_page(to_page, params, opts) {
+    Wishable.page_params = params || {};
+    $.mobile.changePage(to_page, opts);
+  },
+
+  pickup_param: function pickup_param(name) {
+    var from_query = Wishable.query_value(name);
+    return from_query || Wishable.page_params[name];
+  },
+
+  wait: function wait(start_or_stop, message, text_only) {
+    var hide = (start_or_stop == 'stop');
+
+    if (hide) {
+      $.mobile.loading("hide");
+    }
+    else {
+      var show_text = false;
+      if (message) {
+        show_text = true;
+      }
+
+      $.mobile.loading("show", {
+        text:         message,
+        textVisible:  show_text,
+        textonly:     text_only
+      });
+    }
+  }
 };
 
 (function($) {
@@ -72,8 +120,37 @@ var Wishable = Wishable || {
     if (typeof($page) == 'object') {
       // insert the top menu as necessary
       Wishable.unprocessed('.top-menu', $page).each(function() {
-        Wishable.load_template($(this), '#top-menu');
+        var $top_menu = $(this);
+        Wishable.load_template($top_menu, '#top-menu');
+
+        $top_menu.find('.top-menu-content').hide();
+        $top_menu.find('.top-menu-toggle').click(function(e) {
+          e.stopPropagation();
+          e.stopImmediatePropagation();
+          e.preventDefault();
+
+          $top_menu.find('.top-menu-content').toggle();
+
+          if ($top_menu.find('.top-menu-content:visible').length > 0) {
+            $top_menu.find('.top-menu-toggle').html('close');
+            $top_menu.addClass('expanded');
+
+            $('body').on('click.top-menu', function() {
+              $top_menu.find('.top-menu-toggle').click();
+            });
+          }
+          else {
+            $top_menu.find('.top-menu-toggle').html('menu');
+            $top_menu.removeClass('expanded');
+
+            $('body').off('click.top-menu');
+          }
+        });
       });
+
+      if ($page.attr('id') == 'wish') {
+        Wishable.wishes.show(Wishable.pickup_param('wish_id'));
+      }
 
       if ($page.attr('id') == 'wishes') {
         Wishable.wishes.index();
