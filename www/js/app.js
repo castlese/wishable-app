14cdc42add3a8813e3502ec10a62117d46cbf14b
Pete Boucher <petebouch@gmail.com>
2013-10-29 15:44:06 +0100

    Form handlers

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,26 @@ var Wishable = Wishable || {
   initialized:  false,
 
   init:   function init() {
-
+  	$('#login').validate({
+  		submitHandler: function() {
+  			Wishable.api.login(form_params);
+  		}
+  	})
+  	$('#create_user').validate({
+  		submitHandler: function() {
+  			Wishable.api.create_user(form_params);
+  		}
+  	})
+  	$('#create_wish').validate({
+  		submitHandler: function() {
+  			Wishable.api.create_wish(form_params);
+  		}
+  	})
+  	$('#donate').validate({
+  		submitHandler: function() {
+  			Wishable.api.donate(form_params);
+  		}
+  	})
   }
 };
 
